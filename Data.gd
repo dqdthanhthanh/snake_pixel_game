323cf05e8bb6c80ac13a3bf1366ba93689447cd4
Dung Dinh <57259604+dqdthanhthanh@users.noreply.github.com>
2024-04-26 20:05:42 +0700

    add game icon

--- Data.gd
+++ Data.gd
@@ -21,26 +21,3 @@ func get_data():
 static func save_data(data:Dictionary) -> void:
 	var file = FileAccess.open(user_data, FileAccess.WRITE)
 	file.store_string(JSON.stringify(data))
-
-static func get_files_name_path(path : String) -> Array:
-	var files:Array = []
-	var filesPath = DirAccess.get_files_at(path)
-	if filesPath.size() > 0:
-		for i in filesPath:
-			files.append(path + i)
-	return files
-
-static func folder_create(path) -> void:
-	DirAccess.make_dir_absolute(path)
-
-static func folder_remove(path) -> void:
-	DirAccess.remove_absolute(path)
-
-static func str_to_vec2(string := "") -> Vector2:
-	if string:
-		var new_string: String = string
-		new_string = new_string.erase(0, 1)
-		new_string = new_string.erase(new_string.length() - 1, 1)
-		var array: Array = new_string.split(", ")
-		return Vector2(int(array[0]), int(array[1]))
-	return Vector2.ZERO
